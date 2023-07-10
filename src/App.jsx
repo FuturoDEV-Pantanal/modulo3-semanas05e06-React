b8853eff3c253ce2920f6dabcd6c448c375b1bc4
Rosana Rezende <rezende_rosana@hotmail.com>
2023-07-10 19:37:17 -0300

    adiciona novas rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Escolas from "./pages/escolas/Escolas";
+import Login from "./pages/login/Login";
+import NotFound from "./pages/notfound/NotFound";
 
 function App() {
 
@@ -7,7 +9,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Escolas />} />
-        <Route path="/school" element={<Navigate to="/" />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );

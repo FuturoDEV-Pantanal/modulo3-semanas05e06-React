c517f01ba4a75ac05c73eb57d1e16edf719d6d01
Rosana Rezende <rezende_rosana@hotmail.com>
2023-07-12 22:00:47 -0300

    adiciona react testing library

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Escolas from "./pages/escolas/Escolas";
 import Login from "./pages/login/Login";
 import NotFound from "./pages/notfound/NotFound";
+import Contador from "./pages/contador/Contador";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Escolas />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/contador" element={<Contador />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
---
+++ src/pages/contador/Contador.test.jsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Contador from "./Contador";
+
+const usuario = userEvent.setup();
+
+describe("Contador", () => {
+  test("deveria renderizar a logo do React", () => {
+    render(<Contador />);
+
+    const logo = screen.getByRole("img", { name: "React logo" });
+    expect(logo).toBeInTheDocument();
+  });
+
+  test("deveria renderizar o título", () => {
+    render(<Contador />);
+
+    const titulo = screen.getByText("Vite + React");
+    expect(titulo).toBeInTheDocument();
+  });
+
+  test("deveria existir um botão Contador: 0", () => {
+    render(<Contador />);
+
+    const botao = screen.getByRole("button", { name: "Contador: 0" });
+    expect(botao).toBeInTheDocument();
+  });
+
+  test("deveria incrementar o contador ao clicar no botão", async () => {
+    render(<Contador />);
+
+    const botao = screen.getByRole("button", { name: "Contador: 0" });
+    usuario.click(botao);
+
+    const botaoClicado = await screen.findByRole("button", {
+      name: "Contador: 1",
+    });
+    expect(botaoClicado).toBeInTheDocument();
+  });
+});
